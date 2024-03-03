02c1c305f981c595294eb700962d81ea331d9a53
Amtsal99 <amtsal.99@gmail.com>
2024-03-04 06:13:07 +0700

    cek

---
+++ public/js/script.js
@@ -0,0 +1,39 @@
+const navbar = document.getElementById('link_navbar');
+function onToggleMenu(e){
+    if(e.name === 'menu'){
+        e.name = 'close';
+        navbar.classList.toggle('sm:translation-all');
+        navbar.classList.toggle('sm:translate-x-[-30vw]');
+        
+    } 
+    else {
+        e.name = 'menu';
+        navbar.classList.toggle('sm:translation-all');
+        navbar.classList.toggle('sm:translate-x-[-30vw]');      
+    }
+}
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 2,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 2,
+      },
+      "@1.00": {
+        slidesPerView: 2,
+        spaceBetween: 4,
+      },
+      "@1.50": {
+        slidesPerView: 3,
+        spaceBetween: 5,
+      },
+    },
+  });
