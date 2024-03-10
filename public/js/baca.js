ba47d5236a59fdfbea051fe0f90861ee75a9bc9c
Amtsal99 <amtsal.99@gmail.com>
2024-03-10 20:33:41 +0700

    cek8

--- public/js/baca.js
+++ public/js/baca.js
@@ -2,10 +2,11 @@
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.remove("translate-y-1/2");
-    } else {
+    const isBelowViewport = entry.boundingClientRect.top >= window.innerHeight;
+    if (!entry.isIntersecting && isBelowViewport) {
       entry.target.classList.add("translate-y-1/2");
+    } else {
+      entry.target.classList.remove("translate-y-1/2");
     }
   });
 });
--- public/js/script.js
+++ public/js/script.js
@@ -21,10 +21,11 @@ var swiper = new Swiper(".mySwiper", {
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.remove("translate-y-1/2");
-    } else {
+    const isBelowViewport = entry.boundingClientRect.top >= window.innerHeight;
+    if (!entry.isIntersecting && isBelowViewport) {
       entry.target.classList.add("translate-y-1/2");
+    } else {
+      entry.target.classList.remove("translate-y-1/2");
     }
   });
 });
